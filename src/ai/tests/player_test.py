7c7b69ccd768d1dd8d7aff6e16d4688dcc58a7e6
lucktossou <chance.tossou@epitech.eu>
2025-06-07 13:28:50 +0100

    test for player south east and west move

--- src/ai/tests/player_test.py
+++ src/ai/tests/player_test.py
@@ -16,3 +16,23 @@ def test_player_North_move():
     player.move_forward(10, 10)
     assert player.x == 0
     assert player.y == 9
+
+def test_player_South_move():
+    player = Player("momo2", "MTN", 0, 0)
+    player.orientation = "S"
+    player.move_forward(10, 10)
+    assert player.x == 0
+    assert player.y == 1
+
+def test_player_East_move():
+    player = Player("momo3", "MTN", 0, 0)
+    player.orientation = "E"
+    player.move_forward(10, 10)
+    assert player.x == 1
+    assert player.y == 0
+
+def test_player_West_move():
+    player = Player("momo4", "MTN", 0, 0)
+    player.orientation = "W"
+    player.move_forward(10, 10)
+    assert player.x == 9
