d8b53b7117b97d325666f24de779f9ce35d471ec
eyramabiala <christian.abiala@epitech.eu>
2025-06-15 18:04:55 +0100

    correct the initialisation of the gameworld

--- src/GUI/lib/Game_world.hpp
+++ src/GUI/lib/Game_world.hpp
@@ -5,84 +5,51 @@
 #include <string>
 
 struct Resource {
-    int food;
-    int linemate;
-    int deraumere;
-    int sibur;
-    int mendiane;
-    int phiras;
-    int thystame;
-    
+    int food, linemate, deraumere, sibur, mendiane, phiras, thystame;
     Resource() : food(0), linemate(0), deraumere(0), sibur(0), 
                  mendiane(0), phiras(0), thystame(0) {}
 };
 
 struct Player {
-    int id;
-    int x;
-    int y;
-    int orientation;
-    int level;
+    int id, x, y, orientation, level;
     std::string team;
-
-    Player() : id(-1), x(0), y(0), orientation(1), level(1), team("") {}
-    Player(int playerId, int posX, int posY, int orient, int playerLevel, const std::string &teamName)
-        : id(playerId), x(posX), y(posY), orientation(orient), level(playerLevel), team(teamName) {}
+    Player() : id(-1), x(0), y(0), orientation(1), level(1) {}
 };
 
 struct Egg {
-    int id;
-    int x;
-    int y;
+    int id, x, y;
     std::string team;
-    
-    Egg() : id(-1), x(0), y(0), team("") {}
-    Egg(int eggId, int posX, int posY, const std::string &teamName)
-        : id(eggId), x(posX), y(posY), team(teamName) {}
+    Egg() : id(-1), x(0), y(0) {}
 };
 
 struct Tile {
     Resource resources;
     std::vector<int> playerIds;
     std::vector<int> eggIds;
-
-    Tile() {}
 };
 
 class GameWorld {
 private:
-    int m_width;
-    int m_height;
+    int m_width, m_height;
     std::vector<std::vector<Tile>> m_map;
     std::vector<Player> m_players;
     std::vector<Egg> m_eggs;
-    std::vector<std::string> m_teams;
 
 public:
-    GameWorld();
-    ~GameWorld();
-    
     bool initialize(int width, int height);
-    void clear();
-    int getWidth() const;
-    int getHeight() const;
+    int getWidth() const { return m_width; }
+    int getHeight() const { return m_height; }
     const Tile &getTile(int x, int y) const;
-    const std::vector<Player> &getPlayers() const;
-    const std::vector<Egg> &getEggs() const;
-    const std::vector<std::string> &getTeams() const;
-    void addTeam(const std::string &teamName);
+    const std::vector<Player> &getPlayers() const { return m_players; }
+    const std::vector<Egg> &getEggs() const { return m_eggs; }
     void updateTileResources(int x, int y, const Resource &resources);
     void addPlayer(const Player &player);
     void updatePlayerPosition(int playerId, int x, int y, int orientation);
-    void updatePlayerLevel(int playerId, int level);
     void removePlayer(int playerId);
-    Player *findPlayer(int playerId);
     void addEgg(const Egg &egg);
     void removeEgg(int eggId);
-    Egg* findEgg(int eggId);
+    Player *findPlayer(int playerId);
     bool isValidPosition(int x, int y) const;
-    void movePlayerOnMap(int playerId, int oldX, int oldY, int newX, int newY);
-    void moveEggOnMap(int eggId, int oldX, int oldY, int newX, int newY);
 };
 
 #endif
