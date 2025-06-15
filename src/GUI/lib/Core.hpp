07a0338dc74961c84e0ec8e231539977988c49e8
eyramabiala <christian.abiala@epitech.eu>
2025-06-15 17:57:06 +0100

    remove unusefull method

--- src/GUI/lib/Core.hpp
+++ src/GUI/lib/Core.hpp
@@ -2,47 +2,30 @@
 #define CORE_HPP
 
 #include <SFML/Graphics.hpp>
-#include <SFML/Window.hpp>
-#include <SFML/System.hpp>
 #include <memory>
-#include <string>
 
 class Core {
 private:
     std::unique_ptr<sf::RenderWindow> m_window;
-    sf::VideoMode m_videoMode;
-    std::string m_title;
     sf::Event m_event;
-    sf::Clock m_deltaClock;
-    float m_deltaTime;
     bool m_running;
-    
-    int m_windowWidth;
-    int m_windowHeight;
-    int m_framerateLimit;
-    bool m_verticalSyncEnabled;
 
 public:
-    Core();
-    ~Core();
+    Core() : m_running(false) {}
+    ~Core() { shutdown(); }
 
     bool initialize();
     void run();
-    void shutdown();    
+    void shutdown();
     void update();
     void render();
     void pollEvents();
-    sf::RenderWindow& getWindow() const;
-    float getDeltaTime() const;
-    bool isRunning() const;
-    void setFramerateLimit(unsigned int limit);
-    void setVerticalSync(bool enabled);
-    void setWindowTitle(const std::string& title);
-    void handleWindowEvents();
-    
+
+    sf::RenderWindow& getWindow() const { return *m_window; }
+    bool isRunning() const { return m_running; }
+
 private:
-    void initializeWindow();
-    void updateDeltaTime();
+    void handleWindowEvents();
 };
 
 #endif
