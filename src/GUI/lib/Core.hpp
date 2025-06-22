f5b265f9a13a26ea49aa6fdc51384b6199b262d0
eyramabiala <christian.abiala@epitech.eu>
2025-06-22 17:46:55 +0100

    update the core system to use teams information

--- src/GUI/lib/Core.hpp
+++ src/GUI/lib/Core.hpp
@@ -4,28 +4,43 @@
 #include <SFML/Graphics.hpp>
 #include <memory>
 
+class GameWorld;
+class RenderingEngine;
+class InputHandler;
+class AnimationSystem;
+
 class Core {
 private:
     std::unique_ptr<sf::RenderWindow> m_window;
+    std::unique_ptr<GameWorld> m_gameWorld;
+    std::unique_ptr<RenderingEngine> m_renderingEngine;
+    std::unique_ptr<InputHandler> m_inputHandler;
+    std::unique_ptr<AnimationSystem> m_animationSystem;
+    
     sf::Event m_event;
+    sf::Clock m_clock;
     bool m_running;
 
 public:
-    Core() : m_running(false) {}
-    ~Core() { shutdown(); }
+    Core();
+    ~Core();
 
     bool initialize();
     void run();
     void shutdown();
-    void update();
+    void update(float deltaTime);
     void render();
     void pollEvents();
 
-    sf::RenderWindow& getWindow() const { return *m_window; }
+    sf::RenderWindow &getWindow() const { return *m_window; }
     bool isRunning() const { return m_running; }
+    GameWorld *getGameWorld() const { return m_gameWorld.get(); }
 
 private:
     void handleWindowEvents();
+    void initializeTestData();
+    void updateGameLogic(float deltaTime);
+    void renderUI();
 };
 
 #endif
