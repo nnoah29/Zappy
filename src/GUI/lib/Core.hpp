21fcc64c84e938f816cb8628ab3929ef8d33bd00
eyramabiala <christian.abiala@epitech.eu>
2025-06-26 14:17:56 +0100

    update de core system

--- src/GUI/lib/Core.hpp
+++ src/GUI/lib/Core.hpp
@@ -8,12 +8,14 @@ class GameWorld;
 class RenderingEngine;
 class InputHandler;
 class AnimationSystem;
+class PlayerInfoPanel;
 
 class Core {
 private:
     std::unique_ptr<sf::RenderWindow> m_window;
-    std::unique_ptr<GameWorld> m_gameWorld;
+    std::shared_ptr<GameWorld> m_gameWorld;
     std::unique_ptr<RenderingEngine> m_renderingEngine;
+    std::unique_ptr<PlayerInfoPanel> m_playerInfoPanel;
     std::unique_ptr<InputHandler> m_inputHandler;
     std::unique_ptr<AnimationSystem> m_animationSystem;
     
@@ -25,13 +27,12 @@ public:
     Core();
     ~Core();
 
-    bool initialize();
+    bool initialize(std::shared_ptr<GameWorld> gameWorld);
     void run();
     void shutdown();
     void update(float deltaTime);
     void render();
     void pollEvents();
-
     sf::RenderWindow &getWindow() const { return *m_window; }
     bool isRunning() const { return m_running; }
     GameWorld *getGameWorld() const { return m_gameWorld.get(); }
